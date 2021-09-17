6d5c0da77537d77530080497f07468e78572ddba
Ghatos1 <79839168+Ghatos1@users.noreply.github.com>
2021-09-17 11:41:30 +0200

    dqsdqs

--- simulator-script.js
+++ simulator-script.js
@@ -8,21 +8,4 @@ $(document).ready(function(){
             case "suv": $("#vehicle").attr("src", "images/debug_car_3.png"); break;
         }
     })
-    //     {
-    //         case scooter: alert("le moped"); break;
-    //         case citadine: alert("toyota aygo gaming"); break;
-    //         case berline: alert("seudan"); break;
-    //         case suv: alert("4x4 en moins bien"); break;
-    //     }
-    //   });
-    // $("#vehicle").attr('src', 'debug_car').load(function(){this.width});
-    // $("#car-type").on("click", function(){
-    //     switch($(this:nth-child()))
-    //     {
-    //         case 1: $("#vehicle").attr("src", "debug_car"); break;
-    //         case 2: $("#vehicle").attr("src", "debug_car_2"); break;
-    //         case 3: $("#vehicle").attr("src", "debug_car_3"); break;
-    //         case 4: $("#vehicle").attr("src", "debug_car_4"); break;
-    //     }
-    // })
 });
