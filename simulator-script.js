037ad0f72237dabbdb0467dfd94e253e78f06990
Ghatos1 <79839168+Ghatos1@users.noreply.github.com>
2021-09-20 14:06:09 +0200

    sus amogus

--- simulator-script.js
+++ simulator-script.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
-    $('#car-type').change(function(){ 
+    //résout les problèmes liées au rechargement de la page (ex: menu déroulant disant "berline" alors qu'on voie le scooter apparaitre)
+    var vehicle_type = 3;
+    var vehicle_speed = 1;
+    var vehicle_load = 1;
+    var is_seatbelt_on = false;
+    $("#vehicle").attr("src", "images/debug_car.png");
+    $("#car-type").val("berline");
+
+    $("#car-type").change(function(){ 
         switch ($(this).val())
         {
-            case "scooter": $("#vehicle").attr("src", "images/debug_car_4.png"); break;
-            case "citadine": $("#vehicle").attr("src", "images/debug_car_2.png"); break;
-            case "berline": $("#vehicle").attr("src", "images/debug_car.png"); break;
-            case "suv": $("#vehicle").attr("src", "images/debug_car_3.png"); break;
+            case "scooter": $("#vehicle").attr("src", "images/debug_car_4.png"); vehicle_type = 1; break;
+            case "citadine": $("#vehicle").attr("src", "images/debug_car_2.png"); vehicle_type = 2; break;
+            case "berline": $("#vehicle").attr("src", "images/debug_car.png"); vehicle_type = 3; break;
+            case "suv": $("#vehicle").attr("src", "images/debug_car_3.png"); vehicle_type = 4; break;
         }
     })
-    $('#launch').on('click', function(){
+
+    $("#launch").on("click", function(){
+        //désactive les boutons de paramètres lorsqu'on lance la simulation
+        $(".option-buttons").attr("disabled", "true");
+        //animation de la fenêtre des paramètres et de la voiture
         $("#fieldset").transition({x: '-50%', y: '-150%'}, 600, 'ease-in');
-        $("#car-type").attr('disabled', 'true');
-        $("#speed").attr('disabled', 'true');
-        $("#load-type").attr('disabled', 'true');
-        $("#launch").attr('disabled', 'true');
         $("#vehicle").transition({x: '-250%', delay: 250}, 1200, 'cubic-bezier(0.5,0,0.75,0.5)');
     })
 });
