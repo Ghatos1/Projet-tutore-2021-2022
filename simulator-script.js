25daba870857195946e50b0cf6e6ffadaea5a8a0
AdrienGoudeau <adrien.goudeau@gmail.com>
2021-10-01 11:58:26 +0200

    Beautify

--- simulator-script.js
+++ simulator-script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //résout les problèmes liées au rechargement de la page (ex: menu déroulant disant "berline" alors qu'on voie le scooter apparaitre)
     var vehicle_type = 3;
     var vehicle_speed = 1;
@@ -7,22 +7,39 @@ $(document).ready(function(){
     $("#vehicle").attr("src", "images/sedan_spr.svg");
     $("#car-type").val("berline");
 
-    $("#car-type").change(function(){ 
-        switch ($(this).val())
-        {
-            case "scooter": $("#vehicle").attr("src", "images/debug_car_4.png"); vehicle_type = 1; break;
-            case "citadine": $("#vehicle").attr("src", "images/city_car_spr.svg"); vehicle_type = 2; break;
-            case "berline": $("#vehicle").attr("src", "images/sedan_spr.svg"); vehicle_type = 3; break;
-            case "suv": $("#vehicle").attr("src", "images/debug_car_3.png"); vehicle_type = 4; break;
+    $("#car-type").change(function () {
+        switch ($(this).val()) {
+            case "scooter":
+                $("#vehicle").attr("src", "images/debug_car_4.png");
+                vehicle_type = 1;
+                break;
+            case "citadine":
+                $("#vehicle").attr("src", "images/city_car_spr.svg");
+                vehicle_type = 2;
+                break;
+            case "berline":
+                $("#vehicle").attr("src", "images/sedan_spr.svg");
+                vehicle_type = 3;
+                break;
+            case "suv":
+                $("#vehicle").attr("src", "images/debug_car_3.png");
+                vehicle_type = 4;
+                break;
         }
     })
 
-    $("#launch").on("click", function(){
+    $("#launch").on("click", function () {
         //désactive les boutons de paramètres lorsqu'on lance la simulation
         $(".option-buttons").attr("disabled", "true");
         //oui
         //animation de la fenêtre des paramètres et de la voiture
-        $("#fieldset").transition({x: '-50%', y: '-150%'}, 600, 'ease-in');
-        $("#car-attach").transition({x: '-350%', delay: 250}, 1500, 'cubic-bezier(0.5,0,0.75,0.5)');
+        $("#fieldset").transition({
+            x: '-50%',
+            y: '-150%'
+        }, 600, 'ease-in');
+        $("#car-attach").transition({
+            x: '-350%',
+            delay: 250
+        }, 1500, 'cubic-bezier(0.5,0,0.75,0.5)');
     })
 });
