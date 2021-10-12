fb43823934b90fea05c80a54c08ccaa621bfd326
Ghatos1 <79839168+Ghatos1@users.noreply.github.com>
2021-10-12 17:34:54 +0200

    boutons de menu amélioré

--- simulator-script.js
+++ simulator-script.js
@@ -4,26 +4,42 @@ $(document).ready(function () {
     var vehicle_speed = 1;
     var vehicle_load = 1;
     var is_seatbelt_on = false;
+    var is_running_sim = false;
     $("#vehicle").attr("src", "images/sedan_spr.svg");
     $("#car-type").val("berline");
-    // $(".dropdown-content").hide();
-
-    //menus défilants
+    //s"exécute quand un menu défilant est sélectionée
     $(".dropbtn").on("click", function(){
-        // $(".dropdown-content").hide();
-        // $(".dropdown-content").css({"height":"0%"});
         
+        // ferme les menus qui n'ont pas étés sélectionnés
         $(".dropbtn").css({"border-radius":"3vh"});
-        // $(".dropdown-content").transition({
-        //     y: '-100%'
-        // }, 300, 'ease-in-out');
-        // $(this).next().transition({
-        //     y: '0%'
-        // }, 300, 'ease-in-out');
+        $(".dropbtn").css({"border-top-left-radius":"0"});
+        $(".dropbtn").css({"border-top-right-radius":"0"});
+        $(".dropbtn").not(this).next().transition({
+            y: '-100%'
+        }, 300, 'ease-in-out');
+
+        // ouvre le menu sélectionné
+        $(this).next().transition({
+            y: '0%'
+        }, 300, 'ease-in-out');
         $(this).next().css({"transition-duration":"0.2s"});
         $(this).css({"border-bottom-left-radius":"0"});
         $(this).css({"border-bottom-right-radius":"0"});
     })
+
+    //ferme tout les menus quand on clique n'importe où sur la page
+    $(document).mouseup(function(e) {
+        if(! $("#car-type, #speed, #load-type").is(e.target))
+        {
+            $(".dropdown-content").transition({
+                y: '-100%',
+                queue: false
+            }, 300, 'ease-in-out');
+            $(".dropbtn").css({"border-radius":"3vh"});
+            $(".dropbtn").css({"border-top-left-radius":"0"});
+            $(".dropbtn").css({"border-top-right-radius":"0"});
+        }
+    });
 
     //choix de la voiture
     $("#car-type").children().on("click", function(){
@@ -31,80 +47,49 @@ $(document).ready(function () {
         {
             case"Citadine":
                 $("#vehicle").attr("src", "images/city_car_spr.svg");
-                vehicle_type = 1;
+                vehicle_speed = 1;
                 break;
             case"Berline":
                 $("#vehicle").attr("src", "images/sedan_spr.svg");
-                vehicle_type = 2;
+                vehicle_speed = 2;
                 break;
             case"SUV":
-                $("#vehicle").attr("src", "images/suv_spr.svg");
+                $("#vehicle").attr("src", "images/suv_spr.svg")
+                vehicle_speed = 3;
+                break;
+        }
+    })
+
+    //choix de la vitesse
+    $("#speed").children().on("click", function(){
+        switch ($(this).html())
+        {
+            case "50 km/h":
+                vehicle_type = 1;
+                break;
+            case "90 km/h":
+                vehicle_type = 2;
+                break;
+            case "130 km/h":
                 vehicle_type = 3;
                 break;
         }
     })
-    $(document).mouseup(function(e) {
-        if(! $(".dropdown-content").next().is(e.target))
-        {
-            $(this).transition({
-                y: '-100%'
-            }, 300, 'ease-in-out');
-            // if (! $("#car-type").next().is(e.target))
-            // {
-            //     $(this).transition({
-            //         y: '-100%'
-            //     }, 300, 'ease-in-out');
-            // }
-            // if (! $("#speed").next().is(e.target))
-            // {
-            //     $(this).transition({
-            //         y: '-100%'
-            //     }, 300, 'ease-in-out');
-            // }
-            // if (! $("#load-type").next().is(e.target))
-            // {
-            //     $(this).transition({
-            //         y: '-100%'
-            //     }, 300, 'ease-in-out');
-            // }
-        }
-        if ($(".dropdown-content").next().is(e.target))
-        {
-            $(this).transition({
-                y: '0%'
-            }, 300, 'ease-in-out');
-        }
-    });
-    // $("#car-type").change(function () {
-    //     switch ($(this).val()) {
-    //         case "scooter":
-    //             $("#vehicle").attr("src", "images/debug_car_4.png");
-    //             vehicle_type = 1;
-    //             break;
-    //         case "citadine":
-    //             $("#vehicle").attr("src", "images/city_car_spr.svg");
-    //             vehicle_type = 2;
-    //             break;
-    //         case "berline":
-    //             $("#vehicle").attr("src", "images/sedan_spr.svg");
-    //             vehicle_type = 3;
-    //             break;
-    //         case "suv":
-    //             $("#vehicle").attr("src", "images/suv_spr.svg");
-    //             vehicle_type = 4;
-    //             break;
-    //     }
-    // })
 
+    //s"exécute lorsqu'on lance la simulation
     $("#launch").on("click", function () {
+        
         //désactive les boutons de paramètres lorsqu'on lance la simulation
         $(".option-buttons").attr("disabled", "true");
         $(".dropdown-content").hide();
+        $(".dropbtn").css({"background-color":"#777777"});
+        $("#launch").css({"background-color":"#777777"});
+
         //animation de la fenêtre des paramètres et de la voiture
         $("#fieldset").transition({
             x: '-50%',
             y: '-150%'
-        }, 600, 'cubic-bezier(.25,0,.7,-0.4)');
+        }, 6000, 'cubic-bezier(.25,0,.7,-0.4)');
         $("#car-attach").transition({
             x: '-350%',
             delay: 250
