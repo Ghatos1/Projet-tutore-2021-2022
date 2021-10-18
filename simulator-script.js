e24baa2a4776e74d7de70ce88638a78ff12a92dd
Ghatos1 <79839168+Ghatos1@users.noreply.github.com>
2021-10-18 14:00:03 +0200

    zefze

--- simulator-script.js
+++ simulator-script.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     var vehicle_speed = 1;
     var vehicle_load = 1;
     var is_seatbelt_on = false;
-    var is_running_sim = false;
     $("#vehicle").attr("src", "images/sedan_spr.svg");
     $("#car-type").val("berline");
+
     //s"exécute quand un menu défilant est sélectionée
     $(".dropbtn").on("click", function(){
         
@@ -14,22 +14,24 @@ $(document).ready(function () {
         $(".dropbtn").css({"border-radius":"3vh"});
         $(".dropbtn").css({"border-top-left-radius":"0"});
         $(".dropbtn").css({"border-top-right-radius":"0"});
-        $(".dropbtn").not(this).next().transition({
+        $(".drop-clip").not(this).children().transition({
             y: '-100%'
         }, 300, 'ease-in-out');
 
         // ouvre le menu sélectionné
-        $(this).next().transition({
-            y: '0%'
+        $(this).next().children().transition({
+            y: '0%',
+            queue: false
         }, 300, 'ease-in-out');
-        $(this).next().css({"transition-duration":"0.2s"});
+        $(this).next().children().css({"transition-duration":"0.2s"});
         $(this).css({"border-bottom-left-radius":"0"});
         $(this).css({"border-bottom-right-radius":"0"});
     })
 
     //ferme tout les menus quand on clique n'importe où sur la page
     $(document).mouseup(function(e) {
-        if(! $("#car-type, #speed, #load-type").is(e.target))
+
+        if(! $("#car-type, #speed, #load-type, .dropdown-content p").is(e.target))
         {
             $(".dropdown-content").transition({
                 y: '-100%',
@@ -47,15 +49,15 @@ $(document).ready(function () {
         {
             case"Citadine":
                 $("#vehicle").attr("src", "images/city_car_spr.svg");
-                vehicle_speed = 1;
+                vehicle_type = 1;
                 break;
             case"Berline":
                 $("#vehicle").attr("src", "images/sedan_spr.svg");
-                vehicle_speed = 2;
+                vehicle_type = 2;
                 break;
             case"SUV":
                 $("#vehicle").attr("src", "images/suv_spr.svg")
-                vehicle_speed = 3;
+                vehicle_type = 3;
                 break;
         }
     })
@@ -65,13 +67,13 @@ $(document).ready(function () {
         switch ($(this).html())
         {
             case "50 km/h":
-                vehicle_type = 1;
+                vehicle_speed = 1;
                 break;
             case "90 km/h":
-                vehicle_type = 2;
+                vehicle_speed = 2;
                 break;
             case "130 km/h":
-                vehicle_type = 3;
+                vehicle_speed = 3;
                 break;
         }
     })
@@ -89,7 +91,7 @@ $(document).ready(function () {
         $("#fieldset").transition({
             x: '-50%',
             y: '-150%'
-        }, 6000, 'cubic-bezier(.25,0,.7,-0.4)');
+        }, 600, 'cubic-bezier(.25,0,.7,-0.4)');
         $("#car-attach").transition({
             x: '-350%',
             delay: 250
